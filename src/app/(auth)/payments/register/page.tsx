6b3170a3ff342afd1b0066f27d3c745bd24bd618
Carlos Guarda <crguarda95@gmail.com>
2024-01-11 11:58:42 -0500

    fix page

--- src/app/(auth)/payments/register/page.tsx
+++ src/app/(auth)/payments/register/page.tsx
@@ -3,10 +3,7 @@ import { getQueuePayments } from '@/lib/aws-s3/get-queue-payments'
 // ToDo: Establish if want cache for like 2 seconds.
 
 const page = async () => {
-  let paymentsList
-  try {
-    paymentsList = await getQueuePayments()
-  } catch (error) {}
+  const paymentsList = await getQueuePayments()
   if (!paymentsList)
     return (
       <h2 className='h-max w-full text-4xl md:text-7xl text-center font-bold bg-accent-green'>Payment queue empty</h2>
